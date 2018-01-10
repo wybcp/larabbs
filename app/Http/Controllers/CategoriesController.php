426ba466ec7f762fb1e604d11f11202779e7d8f8
Riverside <wangyb65@gmail.com>
2018-01-10 21:56:35 +0800

    Category topic list

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Topic;
+use function compact;
+use Illuminate\Http\Request;
+use function view;
+
+class CategoriesController extends Controller
+{
+    public function show(Category $category)
+    {
+
+        $topics=Topic::where('category_id',$category->id)->with('user','category')->paginate(20);
+
+        return view('topics.index',compact('topics','category'));
+    }
+}
