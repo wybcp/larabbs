c8828eed9591e710f884da4af117c99321c0e9a0
Riverside <wangyb65@gmail.com>
2018-03-08 15:17:45 +0800

    jwt-auth

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -3,37 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use function array_get;
 use function array_set;
+use Auth;
+use function config;
 use Exception;
+use const FILTER_VALIDATE_EMAIL;
+use function filter_var;
+use JWTAuth;
 use Socialite;
+use App\Models\User;
 
 class AuthorizationsController extends Controller
 {
-    public function socialStore(string $type,SocialAuthorizationRequest $request)
+    public function socialStore(string $type, SocialAuthorizationRequest $request)
     {
         if (!in_array($type, ['weixin'])) {
             return $this->response->errorBadRequest();
         }
 
-        $driver=Socialite::driver($type);
+        $driver = Socialite::driver($type);
 
-        try{
+        try {
 
-            if ($code=$request->code){
-                $responce=$driver->getAccessTokenResponce($code);
-                $token=array_get($responce,'access_token');
+            if ($code = $request->code) {
+                $responce = $driver->getAccessTokenResponce($code);
+                $token = array_get($responce, 'access_token');
 
-            }else{
-                $token=$request->access_token;
+            } else {
+                $token = $request->access_token;
 
-                if ($type==='weixin'){
+                if ($type === 'weixin') {
                     $driver->setOpenId($request->openid);
                 }
             }
             $oauthUser = $driver->userFromToken($token);
-        }catch (Exception $e){
+        } catch (Exception $e) {
             return $this->response->errorUnauthorized('参数错误，未获取用户信息');
         }
         switch ($type) {
@@ -49,16 +56,50 @@ class AuthorizationsController extends Controller
                 // 没有用户，默认创建一个用户
                 if (!$user) {
                     $user = User::create([
-                        'name' => $oauthUser->getNickname(),
-                        'avatar' => $oauthUser->getAvatar(),
-                        'weixin_openid' => $oauthUser->getId(),
+                        'name'           => $oauthUser->getNickname(),
+                        'avatar'         => $oauthUser->getAvatar(),
+                        'weixin_openid'  => $oauthUser->getId(),
                         'weixin_unionid' => $unionid,
                     ]);
                 }
 
                 break;
         }
+        $token = Auth::guard('api')->fromUser($user);
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
 
-        return $this->response->array(['token' => $user->id]);
+    public function store(AuthorizationRequest $request)
+    {
+        $user_name = $request->username;
+
+        filter_var($user_name, FILTER_VALIDATE_EMAIL) ? $credentials['email'] = $user_name : $credentials['phone'] = $user_name;
+        $credentials['password'] = $request->password;
+        $ttl=60;
+        if (!$token = Auth::guard('api')->setTTL($ttl)->attempt($credentials)) {
+            return $this->response->errorUnauthorized('用户名或密码错误');
+        }
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        Auth::guard('api')->logout();
+        return $this->response->noContent();
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,8 +9,9 @@ use Illuminate\Notifications\Notifiable;
 use Auth;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasRoles;
     use Notifiable{
@@ -37,6 +38,17 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     public function topics()
     {
         return $this->hasMany(Topic::class);
