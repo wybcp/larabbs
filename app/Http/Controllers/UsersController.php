0c3533b8b4568681f6cdb91d5b45981f2df7e2a9
Riverside <wangyb65@gmail.com>
2018-05-09 15:42:01 +0800

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,10 @@ use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -17,11 +21,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $upload_handler, User $user)
     {
+        $this->authorize('update',$user);
         $data = $request->all();
         if ($request->avatar) {
             $result = $upload_handler->save($request->avatar, 'avatars', $user->id,362);
