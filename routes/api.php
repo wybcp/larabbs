2dcea1be30201a31b83ee293809297d184546eeb
Riverside <wangyb65@gmail.com>
2018-06-12 09:26:37 +0800

    notifications stats

--- routes/api.php
+++ routes/api.php
@@ -90,7 +90,8 @@ $api->version('v1', ["namespace" => "App\Http\Controllers\Api\V1", 'middleware' 
 
 //            通知列表
             $api->get('user/notifications','NotificationsController@index')->name('api.user.notifications.index');
-
+// 通知统计
+            $api->get('user/notifications/stats', 'NotificationsController@stats')->name('api.user.notifications.stats');
 
         });
     });
