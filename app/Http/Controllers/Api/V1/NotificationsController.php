c97b3e194847f6632b2526a15d6ee4825e5b10c3
Riverside <wangyb65@gmail.com>
2018-06-12 09:38:29 +0800

    notifications read

--- app/Http/Controllers/Api/V1/NotificationsController.php
+++ app/Http/Controllers/Api/V1/NotificationsController.php
@@ -21,4 +21,10 @@ class NotificationsController extends Controller
             'unread_count' => $this->user()->notification_count
         ]);
     }
+
+    public function read()
+    {
+        $this->user()->markAsRead();
+        return $this->response->noContent();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -93,6 +93,8 @@ $api->version('v1', ["namespace" => "App\Http\Controllers\Api\V1", 'middleware' 
 // 通知统计
             $api->get('user/notifications/stats', 'NotificationsController@stats')->name('api.user.notifications.stats');
 
+// 标记消息通知为已读
+            $api->patch('user/read/notifications', 'NotificationsController@read')->name('api.user.notifications.read');
         });
     });
 
